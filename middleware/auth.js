a5d4ba3cb080c78235a66926463517044d468920
youngjun114 <35579719+youngjun114@users.noreply.github.com>
2020-07-05 04:29:32 -0700

    Created Profile Schema

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ module.exports = function (req, res, next) {
 
   // Verify token
   try {
-    const decoded = jwt.verify(token, config.get(jwtToken));
+    const decoded = jwt.verify(token, config.get('jwtToken'));
     req.user = decoded.user;
     next();
   } catch (err) {
