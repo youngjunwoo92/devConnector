33db4d74cdcfecff55aeccc2a54b2990d23fa130
youngjun114 <35579719+youngjun114@users.noreply.github.com>
2020-07-06 01:41:53 -0700

    Getting github profile is added using github api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const config = require('config');
+const axios = require('axios');
 const router = express.Router();
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -270,6 +272,27 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   } catch (err) {
     console.log(err.message);
     res.status(500).send('Server Error');
+  }
+});
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`,
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: 'No Github profile found' });
   }
 });
 
