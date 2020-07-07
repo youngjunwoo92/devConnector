73a114952a839e4e8adf853b138cc50586440f1a
youngjun114 <35579719+youngjun114@users.noreply.github.com>
2020-07-06 20:51:13 -0700

    Post routes has been updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/post', require('./routes/api/posts'));
+app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
